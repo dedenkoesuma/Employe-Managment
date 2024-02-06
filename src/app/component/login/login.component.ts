e100fd8ad4f4f4ba4e20dab163adbbb79a50c275
dedenkoesuma <121678764+dedenkoesuma@users.noreply.github.com>
2024-02-06 23:52:38 +0700

    Update login.component.ts

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -3,34 +3,43 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../auth.service';
+import { MatButton } from '@angular/material/button';
+import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [NgIf, FormsModule],
+  imports: [NgIf, FormsModule, MatButton],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
 
-  constructor(private router: Router,private authService: AuthService) {}
+  constructor(private router: Router,private authService: AuthService,private _snackBar: MatSnackBar) {}
 
   login() {
-    // Hardcoded login data
-    const hardcodedUsername = 'admin';
-    const hardcodedPassword = 'password';
-
-    // Check if the entered credentials match the hardcoded ones
-    if (this.username === hardcodedUsername && this.password === hardcodedPassword) {
+    // Validasi kredensial pengguna
+    const isValid = this.authService.validateUser(this.username, this.password);
+    
+    if (isValid) {
       this.authService.login();
-
       this.router.navigate(['/list']);
-      // Redirect to another page or perform any other action upon successful login
     } else {
-      alert('Invalid username or password. Please try again.');
+      this.showSnackBar('Invalid username or password. Please try again.', 'warn');
     }
+  }
+
+  showSnackBar(message: string, panelClass: string) {
+    this._snackBar.open(message, 'Close', {
+      duration: 3000,
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      panelClass: [panelClass],
+    });
   }
 }

a158d0a99b8cb514103a61ff1081b65f149c073e
dedenkoesuma <121678764+dedenkoesuma@users.noreply.github.com>
2024-02-07 00:46:48 +0700

    add users login

---
+++ src/app/shared/models/login.users.ts
@@ -0,0 +1,27 @@
+export interface users {
+    username: string;
+    password: string;
+}
+
+export const dataUser: users[] = [
+    {
+        username: 'admin',
+        password: 'admin',
+    },
+    {
+        username: 'user',
+        password: 'user',
+    },
+    {
+        username: 'user1',
+        password: 'user1',
+    },
+    {
+        username: 'user2',
+        password: 'user2',
+    },
+    {
+        username: 'user3',
+        password: 'user3',
+    }
+]
